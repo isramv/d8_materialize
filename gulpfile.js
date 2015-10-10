385fceab8281a80b465b8349664949eb199e5a9e
Israel Morales <israel@chapterthree.com>
2015-10-10 09:07:28 -0700

    Changing library to materializecss

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
+});
+
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
+
+
+gulp.task('styles', function(){
+  gulp.src(['sass/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('stylesheets/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("sass/**/*.scss", ['styles']);
+  gulp.watch("*.html", ['bs-reload']);
+});
