6ce4ea49763192d87954f99ef42fd980cb7ccf34
Israel Morales <israel.morales@rocket.com.mx>
2016-01-08 14:45:24 -0800

    refactoring code

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
     gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('.stylesheets/'));
+        .pipe(gulp.dest('stylesheets'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('default', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
 });
