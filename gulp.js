c4a01d518f090dc96399e0e77e954a0febe44002
Israel Morales <israel.morales@rocket.com.mx>
2016-01-08 14:08:56 -0800

    gulpfile
    ;

---
+++ gulp.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('.stylesheets/'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
